68445a9a684004babb387a50565761a3f5afeca8
Trend <t2d.yuri@gmail.com>
2014-11-25 13:51:54 +0200

    Add Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "Проект Sample App | " }
 
   describe "Home page" do
 
@@ -11,7 +12,7 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Проект Sample App | Home")
+      expect(page).to have_title("#{base_title}Home")
     end
 
   end
@@ -25,7 +26,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Проект Sample App | Help")
+      expect(page).to have_title("#{base_title}Help")
     end
 
   end
@@ -39,7 +40,21 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Проект Sample App | About Us")
+      expect(page).to have_title("#{base_title}About Us")
+    end
+
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title}Contact")
     end
 
   end
