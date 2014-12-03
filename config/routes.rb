71ba9358dc2b98e6ed7077fcbda1eae3f18cd696
Trend <t2d.yuri@gmail.com>
2014-12-03 14:57:16 +0200

    Finish chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+
   root  'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
